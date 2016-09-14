b9406f0515b9b160325d9d9368fc4cc6f20cb96e
Yulong Tan <swifties@uw.edu>
2016-09-14 14:29:21 -0700

    added plusmults functionality

--- app/src/main/java/yulongproductions/com/overflow/MainActivity.java
+++ app/src/main/java/yulongproductions/com/overflow/MainActivity.java
@@ -5,15 +5,78 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private TextView mResultTextView;
+    private EditText mNum1EditText;
+    private EditText mNum2EditText;
+    private Button mResultButton;
+    private RelativeLayout mLayout;
+    private String symbol;
+    int result = 0;
+
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mResultTextView = (TextView) findViewById(R.id.resultTextView);
+        mNum1EditText = (EditText) findViewById(R.id.num1EditText);
+        mNum2EditText = (EditText) findViewById(R.id.num2EditText);
+        mResultButton = (Button) findViewById(R.id.resultButton);
+        mLayout = (RelativeLayout) findViewById(R.id.mainLayout);
+
+        this.symbol = "";
+        this.result = 0;
+
+        mResultButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String field1 = mNum1EditText.getText().toString().trim();
+                String field2 = mNum2EditText.getText().toString().trim();
+                if (!field1.equals("") && !field2.equals("")) {
+                    int num1 = Integer.parseInt(field1);
+                    int num2 = Integer.parseInt(field2);
+                    MainActivity.this.symbol = "+";
+                    MainActivity.this.result = num1 + num2;
+                    String message = num1 + " " + symbol + " " + num2 + " is " + result;
+                    mResultTextView.setText(message);
+                } else {
+                    Toast.makeText(MainActivity.this, "Please type in a number", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        mResultButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                String field1 = mNum1EditText.getText().toString().trim();
+                String field2 = mNum2EditText.getText().toString().trim();
+                if (!field1.equals("") && !field2.equals("")) {
+                    int num1 = Integer.parseInt(field1);
+                    int num2 = Integer.parseInt(field2);
+                    MainActivity.this.symbol = "*";
+                    MainActivity.this.result = num1 * num2;
+                    String message = num1 + " " + symbol + " " + num2 + " is " + result;
+                    mResultTextView.setText(message);
+                    return true;
+                } else {
+                    Toast.makeText(MainActivity.this, "Please type in a number", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            }
+        });
     }
 
     @Override
@@ -62,7 +125,7 @@ public class MainActivity extends ActionBarActivity {
 
         /* Alternative Switch case
 
-        
+
         switch (item.getItemId()) {
             case R.id.menu_red:
                 if (item.isChecked()) {
