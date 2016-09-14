cda23cb89e82aedabc5229f95db8d032603fe75c
Yulong Tan <swifties@uw.edu>
2016-09-14 14:23:34 -0700

    added code to switch colors

--- app/src/main/java/yulongproductions/com/overflow/MainActivity.java
+++ app/src/main/java/yulongproductions/com/overflow/MainActivity.java
@@ -1,9 +1,11 @@
 package yulongproductions.com.overflow;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -23,16 +25,80 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        RelativeLayout mainLayout = (RelativeLayout) findViewById(R.id.mainLayout);
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.menu_red) {
+            if (item.isChecked()) {
+                item.setChecked(false);
+            } else {
+                item.setChecked(true);
+            }
+            mainLayout.setBackgroundColor(Color.RED);
+        } else if (id == R.id.menu_green) {
+            if (item.isChecked()) {
+                item.setChecked(false);
+            } else {
+                item.setChecked(true);
+            }
+            mainLayout.setBackgroundColor(Color.GREEN);
+        } else if (id == R.id.menu_yellow) {
+            if (item.isChecked()) {
+                item.setChecked(false);
+            } else {
+                item.setChecked(true);
+            }
+            mainLayout.setBackgroundColor(Color.YELLOW);
+        } else if (id == R.id.menu_blue) {
+            if (item.isChecked()) {
+                item.setChecked(false);
+            } else {
+                item.setChecked(true);
+            }
+            mainLayout.setBackgroundColor(Color.BLUE);
+        } else {
+            return super.onOptionsItemSelected(item);
         }
+        return true;
 
-        return super.onOptionsItemSelected(item);
+        /* Alternative Switch case
+
+        
+        switch (item.getItemId()) {
+            case R.id.menu_red:
+                if (item.isChecked()) {
+                    item.setChecked(false);
+                } else {
+                    item.setChecked(true);
+                }
+                mainLayout.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.menu_green:
+                if (item.isChecked()) {
+                    item.setChecked(false);
+                } else {
+                    item.setChecked(true);
+                }
+                mainLayout.setBackgroundColor(Color.GREEN);
+                return true;
+            case R.id.menu_yellow:
+                if (item.isChecked()) {
+                    item.setChecked(false);
+                } else {
+                    item.setChecked(true);
+                }
+                mainLayout.setBackgroundColor(Color.YELLOW);
+                return true;
+            case R.id.menu_blue:
+                if (item.isChecked()) {
+                    item.setChecked(false);
+                } else {
+                    item.setChecked(true);
+                }
+                mainLayout.setBackgroundColor(Color.BLUE);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        */
     }
 }
